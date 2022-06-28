feac493405503397cab486832c5a040e96cf1197
Beto Toro <beto.toro@zemoga.com>
2022-06-28 18:47:57 -0500

    Initial Commit

---
+++ project2-GuessTheFlag/project2_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  project2_GuessTheFlagApp.swift
+//  project2-GuessTheFlag
+//
+//  Created by Beto Toro on 28/06/22.
+//
+
+import SwiftUI
+
+@main
+struct project2_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
