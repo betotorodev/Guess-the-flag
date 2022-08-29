07e485c35038b9da662b4b9db1f5789c6b74dfe0
Beto Toro <betotoro0902@gmail.com>
2022-08-29 10:03:43 -0500

    [feat]: add accesibility

--- project2-GuessTheFlag/ContentView.swift
+++ project2-GuessTheFlag/ContentView.swift
@@ -14,7 +14,7 @@ struct Flagimage: View {
   let isAnimated: Bool
   let selectedFlag: Int
   let flagNumber: Int
-
+  
   var body: some View {
     Image(countries)
       .renderingMode(.original)
@@ -22,7 +22,7 @@ struct Flagimage: View {
       .shadow(radius: 5)
       .rotation3DEffect(.degrees(isAnimated ? animationAmount : 0.0), axis: (x: 0, y: 1, z: 0))
       .opacity(selectedFlag == -1 || flagNumber == selectedFlag ? 1 : 0.25)
-      
+    
   }
 }
 
@@ -35,9 +35,9 @@ struct Title: ViewModifier {
 }
 
 extension View {
-    func titleStyle() -> some View {
-        modifier(Title())
-    }
+  func titleStyle() -> some View {
+    modifier(Title())
+  }
 }
 
 struct ContentView: View {
@@ -45,6 +45,19 @@ struct ContentView: View {
   @State private var showingScore = false
   @State private var scoreTitle = ""
   @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+  let labels = [
+    "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+    "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+    "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+    "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+    "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+    "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+    "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+    "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+    "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+    "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+    "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+  ]
   @State private var correctAnswer = Int.random(in: 0...2)
   @State private var score = 0
   @State private var animationAmount = 0.0
@@ -53,10 +66,10 @@ struct ContentView: View {
   var body: some View {
     ZStack {
       RadialGradient(stops: [
-          .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
-          .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+        .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+        .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
       ], center: .top, startRadius: 200, endRadius: 400)
-        .ignoresSafeArea()
+      .ignoresSafeArea()
       
       VStack {
         
@@ -76,13 +89,14 @@ struct ContentView: View {
             Text(countries[correctAnswer])
               .foregroundColor(.white)
               .font(.largeTitle.weight(.semibold))
-
+            
           }
           ForEach(0..<3) { number in
             Button {
               flagTapped(number)
             } label: {
               Flagimage(countries: countries[number], animationAmount: animationAmount, isAnimated: number == correctAnswer, selectedFlag: selectedflag, flagNumber: number)
+                .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
             }
           }
         }
